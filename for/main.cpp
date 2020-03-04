ab71fb6d3efb6d9e068c913e55c9611fb2b9bcce
Piotr <peterpoter2006@gmail.com>
2020-03-04 17:38:19 +0100

    for

---
+++ for/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Stewardzie, ilu leci pasażerów";
+	int ile;
+	cin>>ile;
+	for(int i= 1;i<=ile;i++){
+		cout<<"Pasazer nr "<<i<<" prosze zapiac pasy"<<endl;
+	}
+}

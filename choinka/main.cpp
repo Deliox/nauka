eb1b7cdc9473d7e3a6a3e5cb4c116ad7f3c31b58
Piotr Garncarz <peterpoter2006@gmail.com>
2020-03-10 21:21:10 +0100

    choinka

--- choinka/main.cpp
+++ choinka/main.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
+
 using namespace std;
 int main(){
 	int i, m;
-	int zakoncz = 3;
-	for(i= 0;i<7;i++){
-	for(m= 0;m<10;m++){
-	cout<<"*";
-	if(m>zakoncz)break;
+	int i2= 1;
+    int spaces=  10;
+    int space;
+
+	for(i= 0;i<8;i++){
+        for(space= 0;space<= spaces;space++){
+            cout<<" ";
+        }
+        for(m= 0;m<i2;m++){
+            cout<<"*";
+        }
+        cout<<endl;
+        spaces-=  1;
+        i2+= 2;
 	}
+	for(i= 0;i<=1;i++){
+        for(spaces= 0;spaces<=9;spaces++){
+            cout<<" ";
+        }
+        cout<<"**"<<endl;
 	}
 }
